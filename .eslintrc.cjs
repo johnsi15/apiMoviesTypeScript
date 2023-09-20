67b19cd9d64c5b69f95b32404bb1829db207d5ed
John Serrano <andrey_201015@hotmail.com>
2023-09-20 16:27:25 -0500

    Add config eslint with standard

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,35 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
+    "env": {
+        "browser": false,
+        "es2021": true
     },
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {}
+    "extends": [
+        "eslint:recommended",
+        "eslint-config-standard-with-typescript",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": "./tsconfig.json"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Movies
+
+¿How does that work?
+
+Require Node.js v18.15.0
+
+* `npm install` to install the dependencies
+* `npm run dev` to run development mode app
+* `npm start` to build production
+
+
+## ESLint
+Quick start `npm init @eslint/config`
+
+Install dependencies:
+
+`npm i eslint-config-standard-with-typescript -D -E`
+
+https://www.npmjs.com/package/eslint-config-standard-with-typescript
+
+
+`.eslintrc.cjs`
+```json
+"extends": [
+  "eslint-config-standard-with-typescript",
+],
+"parserOptions": {
+  "project": "./tsconfig.json"
+},
+```
